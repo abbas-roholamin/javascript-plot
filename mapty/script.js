0646852fe01cfb8598769e2179765c36ae818d9a
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-03-07 18:50:42 +0430

    Update script.js
    Rendering workout form

--- mapty/script.js
+++ mapty/script.js
@@ -10,23 +10,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
       const coords = [latitude, longitude];
-      var map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords).addTo(map).bindPopup('Abbas Roholamin').openPopup();
+      map.on('click', function (e) {
+        mapEvent = e;
+        form.classList.remove('hidden');
+      });
     },
     function () {
       alert("Can't read your location!");
     }
   );
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const { lat, lng } = mapEvent.latlng;
+  const coords = [lat, lng];
+  L.marker(coords).addTo(map).bindPopup('Abbas Roholamin').openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
