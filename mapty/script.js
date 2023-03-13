be13a6b2dd00da2f0b7d8884e21254ff7a23db85
Abbas Roholamin <abbasroholamin76@gmail.com>
2023-03-13 11:56:14 +0430

    Update script.js
    Working create new workout

--- mapty/script.js
+++ mapty/script.js
@@ -11,44 +11,10 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-class Workout {
-  date = new Date();
-  id = Date.now().toString().slice(-10);
-  constructor(coords, distance, duration) {
-    this.coords = coords; // [lat, lng]
-    this.distance = distance; // in km
-    this.duration = duration; // in min
-  }
-}
-
-class Runing extends Workout {
-  constructor(coords, distance, duration, cadence) {
-    super(coords, distance, duration);
-    this.cadence = cadence;
-    this.calcPace();
-  }
-
-  calcPace() {
-    this.pace = this.duration / this.distance;
-    return this.pace;
-  }
-}
-
-class Cycling extends Workout {
-  constructor(coords, distance, duration, elevationGain) {
-    super(coords, distance, duration);
-    this.elevationGain = elevationGain;
-    this.calcSpeed();
-  }
-
-  calcSpeed() {
-    this.speed = this.distance / (this.duration / 60);
-    return this.speed;
-  }
-}
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -59,7 +25,7 @@ class App {
   _getPosition() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        this._getPosition.bind(this),
+        this._loadMap.bind(this),
         function () {
           alert("Can't read your location!");
         }
@@ -86,6 +52,10 @@ class App {
     inputDistance.focus();
   }
 
+  _hideForm() {
+    form.classList.add('hidden');
+  }
+
   _toggleElevationFeild() {
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
@@ -93,9 +63,109 @@ class App {
 
   _newWorkout(e) {
     e.preventDefault();
+
+    //GET FORM DATA
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
     const { lat, lng } = this.#mapEvent.latlng;
-    const coords = [lat, lng];
-    L.marker(coords).addTo(this.#map).bindPopup('Abbas Roholamin').openPopup();
+    let workout;
+
+    // VALIDATION
+
+    // IF WORKOUT IS RUNNING CREATE RUNNING OBJECT
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      if (
+        !this._validInputs(distance, duration, cadence) ||
+        !this._CheckIsPositive(distance, duration, cadence)
+      ) {
+        return alert('Input has to be positive number!');
+      }
+
+      workout = new Runing([lat, lng], distance, duration, cadence);
+    }
+
+    // IF WORKOUT IS CYCLING CREATE CYCLING OBJECT
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !this._validInputs(distance, duration, elevation) ||
+        !this._CheckIsPositive(distance, duration)
+      ) {
+        return alert('Input has to be positive number!');
+      }
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+    // ADD WORKOUT TO WORKOUT ARRAY
+    this.#workouts.push(workout);
+
+    // REANDER WORKOUT MARKER
+    this._renderWorkoutMarker(workout);
+
+    // UPDATE WORKOUT LIST
+
+    // HIDE FORM
+    this._hideForm();
+  }
+
+  _renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      )
+      .setPopupContent(`${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}`)
+      .openPopup();
+  }
+
+  _validInputs = (...inputs) => inputs.every(input => Number.isFinite(input));
+
+  _CheckIsPositive = (...inputs) => inputs.every(input => input > 0);
+}
+
+class Workout {
+  date = new Date();
+  id = Date.now().toString().slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Runing extends Workout {
+  #type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  #type = 'cycling';
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
   }
 }
 
